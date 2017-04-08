dfb3f27ee39ba98d708da7afd07cfdb4a10a2f1f
DavidMBP <steadypathapp@gmail.com>
2017-04-08 17:03:09 -0500

    Added minify gulp progess

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+const jsFiles = [
+  "js/utils/MLoader.js",
+  "js/utils/alias.js",
+  "js/utils/helpers.js",
+  "js/sprites/Bubble.js",
+  "js/utils/BubbleSpawner.js",
+  "js/ui/Button.js",
+  "js/sections/GamePlay.js",
+  "js/sections/PauseSection.js",
+  "js/scenes/StartScene.js",
+  "js/scenes/PlayScene.js",
+  "js/main.js"
+]
+
+gulp.task('minifyJS', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('index.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+});
